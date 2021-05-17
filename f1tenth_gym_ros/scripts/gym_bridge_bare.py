a88de0c4fd27b5b86bf6a134ea1ac2ba78241a81
Kubilay Eksioglu <kubilayeksioglu@gmail.com>
2021-05-17 20:24:40 +0300

    - Enables running agents in F1Tenth Python3 environment instead of C++ backend.

--- f1tenth_gym_ros/scripts/gym_bridge_bare.py
+++ f1tenth_gym_ros/scripts/gym_bridge_bare.py
@@ -26,19 +26,20 @@ class Agent(object):
         self.scan_pub = rospy.Publisher(self.scan_topic, LaserScan, queue_size=1)
         self.drive_sub = rospy.Subscriber(self.drive_topic, AckermannDriveStamped, drive_callback, queue_size=1)
 
+        # RViz Topics
+        self.odom_topic = '/%s/odom' % self.id
+        self.opp_odom_topic = '/%s/opp_odom' % self.id
+
 
 class GymBridge(object):
 
     def __init__(self):
         # get env vars
-        self.race_scenario = os.environ.get('RACE_SCENARIO')
+        self.race_scenario = int(os.environ.get('RACE_SCENARIO', 0))
         self.agents = []
-        if self.race_scenario is None:
-            print('Race scenario not set! Using single car timed trial as default.')
-            self.race_scenario = 0
-            self.agents.append(Agent(os.environ.get("EGO_ID"), self.drive_callback))
-        else:
-            self.race_scenario = int(self.race_scenario)
+
+        self.agents.append(Agent(os.environ.get("EGO_ID"), self.drive_callback))
+        if self.race_scenario > 0:
             self.agents.append(Agent(os.environ.get('OPP_ID'), self.opp_drive_callback))
 
         # Topic Names
@@ -67,24 +68,28 @@ class GymBridge(object):
         # init gym backend
         poses = [[-1.25 + (i * 0.75), 0., np.radians(90)] for i in range(driver_count)]
         self.obs, _, self.done, _ = self.env.reset(poses=np.array(poses))
+        self.env.render()
 
         for i, scan in enumerate(self.obs['scans']):
             self.agents[i].scan = scan
 
         for i, collision in enumerate(self.obs['collisions']):
-            self.agents[i].collision = collision
+            self.agents[i].collision = bool(collision)
 
         # Timer
         self.timer = rospy.Timer(rospy.Duration(0.004), self.timer_callback)
         self.drive_timer = rospy.Timer(rospy.Duration(0.01), self.drive_timer_callback)
+
+    def spin(self):
+        while not rospy.core.is_shutdown():
+            rospy.rostime.wallsleep(0.04)
+            self.env.render(mode='human')
 
     def update_sim_state(self):
         for i, scan in enumerate(self.obs['scans']):
             self.agents[i].scan = scan
 
     def drive_callback(self, drive_msg):
-        import time
-        print (time.time(), drive_msg.drive.steering_angle)
         self.agents[0].requested_speed = drive_msg.drive.speed
         self.agents[0].requested_steer = drive_msg.drive.steering_angle
         self.agents[0].drive_published = True
@@ -96,10 +101,11 @@ class GymBridge(object):
 
     def drive_timer_callback(self, timer):
         published = all([a.drive_published for a in self.agents])
+
         if published:
-            actions = [[a.requested_speed, a.requested_steer] for a in self.agents]
-            self.obs, step_reward, self.done, info = self.env.step(np.array(actions))
-            self.update_sim_state()
+            actions = [[a.requested_steer, a.requested_speed] for a in self.agents]
+            self.obs, _, self.done, _ = self.env.step(np.array(actions))
+        self.update_sim_state()
 
     def timer_callback(self, timer):
         ts = rospy.Time.now()
@@ -131,7 +137,7 @@ class GymBridge(object):
         info.ego_collision = self.agents[0].collision
         info.ego_elapsed_time = self.obs['lap_times'][0]
         info.ego_lap_count = self.obs['lap_counts'][0]
-        info.opp_collision = self.agents[1].collision if len(self.agents) > 1 else False
+        info.opp_collision = self.agents[1].collision if len(self.agents) > 1 else 0
         info.opp_elapsed_time = self.obs['lap_times'][1] if len(self.agents) > 1 else 0.0
         info.opp_lap_count = self.obs['lap_counts'][1] if len(self.agents) > 1 else 0
         self.info_pub.publish(info)
@@ -140,4 +146,4 @@ class GymBridge(object):
 if __name__ == '__main__':
     rospy.init_node('gym_bridge')
     gym_bridge = GymBridge()
-    rospy.spin()
+    gym_bridge.spin()
